1d31fd201b3ea88bb04713d8ac2817dcf88ac11e
NickolasKnihs <Nickolas_knihs@estudante.sesisenai.org.br>
2024-02-26 21:48:13 -0300

    1 a 21

--- atividade15.js
+++ atividade15.js
@@ -7,7 +7,6 @@ function main(palavra1, palavra2) {
         return false;
     }
 
-
     // Converte as palavras para letras minúsculas para ignorar a diferença de capitalização
     palavra1 = palavra1.toLowerCase();
     palavra2 = palavra2.toLowerCase();
@@ -17,18 +16,15 @@ function main(palavra1, palavra2) {
     const contador1 = {};
     const contador2 = {};
 
-
     // Preenche os contadores para a primeira palavra
     for (let letra of palavra1) {
         contador1[letra] = (contador1[letra] || 0) + 1;
     }
 
-
     // Preenche os contadores para a segunda palavra
     for (let letra of palavra2) {
         contador2[letra] = (contador2[letra] || 0) + 1;
     }
-
 
     // Compara os contadores para verificar se as palavras têm as mesmas letras com a mesma frequência
     for (let chave in contador1) {
@@ -37,14 +33,31 @@ function main(palavra1, palavra2) {
         }
     }
 
-
     return true;
+
+    /*
+      
+    const palavra1Organizada = palavra1
+    .toLowerCase()
+    .split(' ')
+    .join('')
+    .split('')
+    .sort()
+    .join();
+
+    const palavra2Organizada = palavra2
+    .toLowerCase()
+    .split(' ')
+    .join('')
+    .split('')
+    .sort()
+    .join();
+
+    return palavra1Organizada === palavra2Organizada
+
+    */
+
 }
-
-
-
-
-
 
 console.log(main('listen', 'silent'));
 console.log(main('hello', 'world'));
